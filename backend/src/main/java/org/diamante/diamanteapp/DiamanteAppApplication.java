d9dc71eebf7d31e84d15de4ed0f588bd70a76fe0
Joselino Fermin <tucang@gmail.com>
2021-01-28 10:38:55 -0300

    Project created

---
+++ backend/src/main/java/org/diamante/diamanteapp/DiamanteAppApplication.java
@@ -0,0 +1,13 @@
+package org.diamante.diamanteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamanteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiamanteAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/diamante/diamanteapp/DiamanteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.diamante.diamanteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiamanteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
